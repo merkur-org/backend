36af8ac4caca7917d505e68b6d73d629bec00d18
Gabriel <gprando55@gmail.com>
2021-05-05 22:42:45 -0300

    chore: implements filter for products :construction:

--- src/modules/products/dtos/IPaginationInListDTO.ts
+++ src/modules/products/dtos/IPaginationInListDTO.ts
@@ -1,7 +1,32 @@
 import { TList } from '@modules/lists/infra/typeorm/entities/List';
 import IPaginationDTO from '@shared/dtos/IPaginationDTO';
 
-export default interface IPaginationInListDTO extends IPaginationDTO {
+interface IFilter {
+  name?: string;
+  image?: string;
+  nutritional_information?: string;
+  observation?: string;
+  unit_sale?: string;
+  category?: string;
+  unit_buy?: string;
+  fraction_buy?: number;
+  fraction_sale?: number;
+  cost_price?: number;
+  sale_price?: number;
+  wholesale_price?: number;
+  organic?: boolean;
+  highlights?: boolean;
+}
+
+interface ISort {
+  sort_by?: string;
+  order?: 'ASC' | 'DESC';
+}
+
+export default interface IPaginationInListDTO
+  extends IPaginationDTO,
+    IFilter,
+    ISort {
   type: TList;
   date: Date;
 }
